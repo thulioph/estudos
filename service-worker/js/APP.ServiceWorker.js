8e334a7060dd932f02615c48165fc9a694dfa0fd
thulioph <thulioph@gmail.com>
2015-05-29 16:06:57 -0300

    Reescreve o service worker por completo.

--- service-worker/js/APP.ServiceWorker.js
+++ service-worker/js/APP.ServiceWorker.js
@@ -1,6 +1,20 @@
+// =============
+// Tudo é baseado em promisses
+// =============
+
+
 // Dispara durante a instalação, baixando os recursos.
 this.oninstall = function(event) {
-  console.log('Instalou!');
+  
+  event.waitUntil(
+    caches.open('aplicacao').then(function(cache) {
+      return cache.addAll([
+        '/index.html',
+        '/css/style.css',
+        '/js/APP.Request.js'
+      ]);
+    })
+  );
 };
 
 // Terminou de baixar e vai ficar valendo.
@@ -10,5 +24,15 @@ this.onactive = function(event) {
 
 // Dispara todas as requisições da minha página (intercepta a url).
 this.onfetch = function(event) {
-  console.log(event.request.url);
+  console.log('URL -> ', event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      // caso não tenha nada no cache, dispara a requisição e pega na rede.
+      return response || event.default();
+    }).catch(function() { // tratando erro da promisses com o catch (fallback)
+      // exibe algo que realmente esteja no cache
+      return caches.match('/contato.html');
+    });
+  );
 }
