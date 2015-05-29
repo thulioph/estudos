2f2c132c6f7e76bf3b4575be40228520a57583e0
thulioph <thulioph@gmail.com>
2015-05-29 15:14:02 -0300

    Adiciona o método fetch.

--- service-worker/js/APP.ServiceWorker.js
+++ service-worker/js/APP.ServiceWorker.js
@@ -1,9 +1,19 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    fetchStuffAndInitDatabases()
-  );
+this.addEventListener('install', function(event) {
+  var myResources = new Cache (
+    "/index.html",
+    "/css/style.css",
+    "/images/logo.jpeg"
+    );
+
+  event.waitUntil(myResources.ready());
+  
+  caches.set('caches-v1', myResources);
 });
 
-self.addEventListener('activate', function(event) {
+this.addEventListener('fetch', function(event) {
+  console.log('Url requisitada: ', event.request.url);
+});
+
+this.addEventListener('activate', function(event) {
   console.log('Activate: ', event);
 });
