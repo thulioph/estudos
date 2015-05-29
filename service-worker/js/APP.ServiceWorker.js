fb0b5e8d46bfc49b63133b8880e9a89b97a24f13
thulioph <thulioph@gmail.com>
2015-05-29 15:09:13 -0300

    Adiciona nova maneira para instalar e ativar.

--- service-worker/js/APP.ServiceWorker.js
+++ service-worker/js/APP.ServiceWorker.js
@@ -1,40 +1,9 @@
-var APP = APP || {};
-APP.ServiceWorker = {
-  setUp: function() {
-    this.listeners();
-  },
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    fetchStuffAndInitDatabases()
+  );
+});
 
-  listeners: function() {
-    var that = this;
-
-    window.addEventListener('install', that.onInstall, false);
-    window.addEventListener('fetch', that.onFetch, false);
-    window.addEventListener('activate', that.onActive, false);
-  },
-
-  onInstall: function(event) {
-    // var version, myResources;
-
-    console.log('Instal: ', event);
-
-    // version = 1;
-    // myResources = new Cache(
-    //   '/index.html', 
-    //   '/images/logo.jpeg'
-    // );
-
-    // event.waitUntil(fetchStuffAndInitDatabases());
-    // cache.set('caches-' + version, myResources);
-  },
-
-  onFetch: function(event) {
-    console.log('Fetch: ', event);
-    // console.log('Url requisitada: ', event.request);
-
-    // event.respondWith(new Response("Hello world!"));
-  },
-
-  onActive: function(event) {
-    console.log('Active: ', event);
-  }
-}
+self.addEventListener('activate', function(event) {
+  console.log('Activate: ', event);
+});
