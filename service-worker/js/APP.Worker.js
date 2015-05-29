09b01d040604c54588028b47bb4cf1fb8f273a0d
thulioph <thulioph@gmail.com>
2015-05-29 18:08:40 -0300

    Altera o módulo principal para load.

--- service-worker/js/APP.Worker.js
+++ service-worker/js/APP.Worker.js
@@ -1,18 +1,19 @@
 var APP = APP || {};
 APP.Worker = {
   setUp: function() {
-    this.register();
+    this.init();
   },
 
   init: function() {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/estudos/service-worker/js/APP.ServiceWorker.js', {
-        scope: '/estudos/service-worker/js/'
-      }).then(function(reg) {
+      navigator.serviceWorker.register('./sw.js', { scope: './'})
+      .then(function(reg) {
         console.log('Yey!', reg);
       }).catch(function(err) {
         console.warn('Boo!', err);
       });
+    } else {
+      console.warn('Seu navegador não suporta service worker.');
     }
   },
 
