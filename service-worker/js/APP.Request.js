534388ae6db7605e5506b2cc63f1cd4d3c2bda26
thulioph <thulioph@gmail.com>
2015-05-29 03:56:54 -0300

    Adiciona estrutura inicial para o template e request.

--- service-worker/js/APP.Request.js
+++ service-worker/js/APP.Request.js
@@ -5,6 +5,10 @@ APP.Request = {
   },
 
   ajax: function() {
+    var that;
+
+    that = this;
+
     $.ajax({
       url: 'https://api.github.com/gists?callback=?',
       type: 'GET',
@@ -15,12 +19,28 @@ APP.Request = {
       },
 
       success: function(data) {
-        console.log(data);
+        that.templateGist(data);
       },
 
       error: function(error) {
         console.warn('Error: ', error);
       }
     });
+  },
+
+  templateGist: function(data) {
+    var source, template, gists, dataLengh, gistLink, owner, userImage, profile;
+
+    source = document.getElementById('gist-template').innerHTML;
+    template = Handlebars.compile(source);
+    gists = [];
+    dataLengh = data.length;
+
+    for (var i = 0; i <= dataLengh; i++) {
+      gistLink = data[i].html_url, 
+      owner = data[i].owner.login,
+      userImage = data[i].owner.avatar_url,
+      profile = data[i].owner.url;
+    }
   }
 }
