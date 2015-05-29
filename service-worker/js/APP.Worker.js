18d331620492d40a5f11484d1f5b5671150ed4e6
thulioph <thulioph@gmail.com>
2015-05-29 17:55:51 -0300

    Adiciona polyfill e altera a estrutura de algumas coisas.

--- service-worker/js/APP.Worker.js
+++ service-worker/js/APP.Worker.js
@@ -17,10 +17,112 @@ APP.Worker = {
   },
 
   register: function() {
-    navigator.serviceWorker.register('/estudos/service-worker/js/APP.ServiceWorker.js').then(function() {
-      console.log('Registrado!');
-    }, function(error) {
-      console.log('Problemas, ', error);
+    var that;
+
+    that = this;
+
+    if ('serviceWorker' in navigator && 'caches' in window) {
+      navigator.serviceWorker.register('./sw.js', {scope: './'})
+      .then(that.waitUntilInstalled)
+      .then(that.initializeUI)
+      .catch(function(error) {
+        console.warn('Problema: ', error);
+      })
+    } else {
+      console.warn('Seu navegador não suporta service worker.');
+    }
+
+  },
+
+  waitUntilInstalled: function(registration) {
+    return new Promise(function(resolve, reject) {
+      if (registration.installing) {
+        registration.installing.addEventListener('statechange', function(event) {
+          if (event.target.state == 'installed') {
+            resolve();
+          } else if (event.target.state == 'redundant') {
+            reject();
+          }
+        })
+      } else {
+        resolve();
+      }
     });
+  },
+
+  initializeUI: function() {
+    var that = this;
+
+    document.querySelector('#files').style.display = 'block';
+
+    document.querySelector('#add').addEventListener('click', function() {
+      var url = document.querySelector('#url').value;
+
+      if (url) {
+        that.addUrlToCache(url);
+      }
+    });
+
+    showList();
+  },
+
+  addUrlToCache: function(url) {
+    var that = this;
+
+    window.fetch(url, {mode: 'no-cors'}).then(function(response) {
+      if (response.status < 400) {
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(url, response).then(that.showList);
+        });
+      }
+    }).catch(function(error) {
+      console.log('Error:, ', error);
+    });
+  },
+
+  showList: function() {
+    var that = this;
+
+    while(cacheEntriesUl.firstChild) {
+      cacheEntriesUl.removeChild(cacheEntriesUl.firstChild);
+    }
+
+    window.caches.keys().then(function(cacheNames) {
+      cacheNames.forEach(function(cacheName) {
+        window.caches.open(cacheName).then(function(cache) {
+          cache.keys().then(function(requests) {
+            requests.forEach(function(requests) {
+              that.addRequestToList(cacheName, request);
+            });
+          });
+        });
+      })
+    });
+  },
+
+  addRequestToList: function(cacheName, request) {
+    var that = this;
+    var url = request.url;
+
+    var spanElement = document.createElement('span');
+        spanElement.textContent = url;
+
+    var buttonElement = document.createElement('button');
+        buttonElement.textContent = 'Remove';
+        buttonElement.dataset.url = url;
+        buttonElement.dataset.cacheName = cacheName;
+        
+        buttonElement.addEventListener('click', function() {
+          that.removeCachedResponse(this.dataset.cacheName, this.dataset.url).then(function() {
+            var parent = this.parentNode;
+            var grandParent = parent.parentNode;
+            grandParent.removeChild(parent);
+          }.bind(this));
+        });
+  },
+
+  handler: function() {
+    var CACHE_NAME = 'window-cache-v1';
+    // var cacheEntriesUl = 
   }
 }
---
+++ service-worker/sw.js
@@ -0,0 +1,55 @@
+importScripts('serviceworker-cache-polyfill.js');
+
+var cache_version = 1;
+
+var current_caches = {
+  prefetch: 'window-cache-v' + cache_version
+};
+
+self.addEventListener('install', function(event) {
+  var urlsToPrefetch = [
+    './index.html',
+    './contato.html',
+    './css/style.css',
+    './images/logo.jpeg',
+    './bower_components/bootstrap/dist/css/bootstrap-theme.css',
+    './bower_components/bootstrap/dist/css/bootstrap.css',
+    './bower_components/jquery/dist/jquery.js',
+    './bower_components/handlebars/handlebars.js',
+    './bower_components/APP/index.js'
+  ];
+
+  console.log('Install event. Resources to prefetch: ', urlsToPrefetch);
+
+  event.waintUntil(
+    caches.open(current_caches['prefetch'])
+    .then(function(cache) {
+      return cache.addAll(urlsToPrefetch.map(function(urlsToPrefetch) {
+        return new Request(urlsToPrefetch, {mode: 'no-cors'});
+      })).then(function() {
+        console.log('All resources have been fetched and cached.');
+      });
+    }).catch(function(error) {
+      console.log('Pre-fetching failed: ', error);
+    });
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var expectedCacheNames = Object.keys(current_caches).map(function(key) {
+    return current_caches[key];
+  });
+
+  event.waintUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (expectedCacheNames.indexOf(cacheName) == -1) {
+            console.log('Deleting out of date cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
