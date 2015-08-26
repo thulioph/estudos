9cd85b70adc2c189467ed803700a990470aa917e
thulioph <thulioph@gmail.com>
2015-08-26 17:38:11 -0300

    Remove alguns comentários não utilizados.

--- nodejs/socket-io/index.js
+++ nodejs/socket-io/index.js
@@ -5,15 +5,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
-// app.get('/', function(req, res) {
-//   res.sendFile(__dirname + '/index.html');
-// });
-
-// http.listen(3000, function() {
-//   console.log('listening on *:3000');
-// });
-
-
 // 
 // After install socket.io
 // 
