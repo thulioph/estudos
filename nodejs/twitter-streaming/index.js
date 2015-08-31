27f031cbe8b187c514b3793a4a12c95614037b3f
thulioph <thulioph@gmail.com>
2015-08-31 16:11:02 -0300

    Removes trash code.

--- nodejs/twitter-streaming/index.js
+++ nodejs/twitter-streaming/index.js
@@ -1,15 +1,8 @@
-// var express = require('express');
-// var app = express();
-// var http = require('http');
-// var server = http.createServer(app);
-// var io = require('socket.io').listen(server);
-
 var app = require('express')();
 var http = require('http').Server(app);
 var Twit = require('twit');
 var io = require('socket.io')(http);
 
-// server.listen(8080);
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
@@ -46,8 +39,6 @@ io.sockets.on('connection', function(socket) {
       lng: tweet.geo.coordinates[1],
       url: 'https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.user.id_str
     };
-
-    // var lat, lng;
 
     io.sockets.emit('stream', tweetJSON);
     console.log(tweet);
