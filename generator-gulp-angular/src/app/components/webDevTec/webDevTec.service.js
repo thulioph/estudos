3d9dd2e6e5d84b40f0cbe1231212c71506060c6e
thulioph <thulioph@gmail.com>
2016-06-08 06:29:16 -0300

    Adiciona helloworld com generator gulp angular

---
+++ generator-gulp-angular/src/app/components/webDevTec/webDevTec.service.js
@@ -0,0 +1,66 @@
+export class WebDevTecService {
+  constructor () {
+    'ngInject';
+
+    this.data = [
+      {
+        'title': 'AngularJS',
+        'url': 'https://angularjs.org/',
+        'description': 'HTML enhanced for web apps!',
+        'logo': 'angular.png'
+      },
+      {
+        'title': 'BrowserSync',
+        'url': 'http://browsersync.io/',
+        'description': 'Time-saving synchronised browser testing.',
+        'logo': 'browsersync.png'
+      },
+      {
+        'title': 'GulpJS',
+        'url': 'http://gulpjs.com/',
+        'description': 'The streaming build system.',
+        'logo': 'gulp.png'
+      },
+      {
+        'title': 'Jasmine',
+        'url': 'http://jasmine.github.io/',
+        'description': 'Behavior-Driven JavaScript.',
+        'logo': 'jasmine.png'
+      },
+      {
+        'title': 'Karma',
+        'url': 'http://karma-runner.github.io/',
+        'description': 'Spectacular Test Runner for JavaScript.',
+        'logo': 'karma.png'
+      },
+      {
+        'title': 'Protractor',
+        'url': 'https://github.com/angular/protractor',
+        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
+        'logo': 'protractor.png'
+      },
+      {
+        'title': 'Material Design Lite',
+        'url': 'http://www.getmdl.io/',
+        'description': 'Add a Material Design look and feel to your websites.',
+        'logo': 'material-design-lite.png'
+      },
+      {
+        'title': 'Sass (Ruby)',
+        'url': 'http://sass-lang.com/',
+        'description': 'Original Syntactically Awesome StyleSheets implemented in Ruby',
+        'logo': 'ruby-sass.png'
+      },
+      {
+        'title': 'ES6 (Babel formerly 6to5)',
+        'url': 'https://babeljs.io/',
+        'description': 'Turns ES6+ code into vanilla ES5, so you can use next generation features today.',
+        'logo': 'babel.png'
+      }
+    ];
+  }
+
+  getTec() {
+    return this.data;
+  }
+}
