c9db7ab5cb45686321277239a7ddcc1efb7dd52a
Thulio Philipe <thulioph@gmail.com>
2017-02-20 21:32:51 -0300

    Obtém a listagem de repositórios de um determinado usuário.

--- angular2/app/components/user.component.ts
+++ angular2/app/components/user.component.ts
@@ -14,6 +14,7 @@ export class UserComponent implements OnInit {
   private users: UserComponent[];
   private apiUrl = `https://api.github.com`;
   private userInfo = [];
+  private userRepo = [];
 
   constructor(
     private _user: UserService;
@@ -22,9 +23,23 @@ export class UserComponent implements OnInit {
 
   ngOnInit() {
     let route = this._router.parent.lastNavigationAttempt;
-    let api_url = `${this.apiUrl}${route}`
+    let api_url = `${this.apiUrl}${route}`;
 
     this._user.get(api_url)
-      .then((result) => this.userInfo.push(result));
+      .then(_handleInfo.bind(this))
+      .then(_handleRepo.bind(this))
+      .catch(_handleError);
+
+    function _handleInfo(result) {
+      return this.userInfo.push(result);
+    }
+
+    function _handleRepo() {
+      this._user.get(`${api_url}/repos`).then((result) => this.userRepo = result);
+    }
+
+    function _handleError(err) {
+      return console.error(err);
+    }
   }
 }
